d66130eebb060b87a43b55e0004837da44361276
Alexander Roberts <detrohutt@gmail.com>
2017-12-15 12:36:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const { injectBabelPlugin } = require('react-app-rewired');
 
+function rewireInlineImportGraphqlAst(
+  config,
+  env,
+  graphqlPluginOptions = {}
+) {
+  return injectBabelPlugin(
+    ['inline-import-graphql-ast', graphqlPluginOptions],
+    config
+  );
+}
+
+module.exports = rewireInlineImportGraphqlAst;
